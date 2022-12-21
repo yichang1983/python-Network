06a1769bb70285f925b019adab43c54b2ae56a2d
yichang1983 <yichang.thomas@gmail.com>
2022-12-21 15:22:11 +0000

    Arista show and config commands

--- Chapter3/Arista/pyeapi_1.py
+++ Chapter3/Arista/pyeapi_1.py
@@ -1,5 +1,9 @@
 import pyeapi
 import pprint
-
+#show command
 arista1 = pyeapi.connect_to('Arista1')
+pprint.pprint(arista1.enable('show hostname'))
+
+#config command, change hostname
+arista1.config('hostname arista1-new')
 pprint.pprint(arista1.enable('show hostname'))
